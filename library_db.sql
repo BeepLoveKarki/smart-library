ca17f7d3a8c81b1438a47d9e5da5c2a1ca2c85e1
Beep Love <biplabkarki04@gmail.com>
2018-02-28 17:39:48 +0545

    Smart libary database SQL file (MYSQL DB)

--- library_db.sql
+++ library_db.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Feb 28, 2018 at 12:31 PM
--- Server version: 10.1.30-MariaDB
--- PHP Version: 7.2.1
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
